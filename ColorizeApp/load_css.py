2bf262085ba59523a96348902fbc3048c71b4678
Alice Pham <phamthaiphuongnghi@gmail.com>
2021-05-28 03:57:57 -0700

    Update load_css.py
    load scss file

--- ColorizeApp/load_css.py
+++ ColorizeApp/load_css.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-def local_css(file_name):
+def local_scss(file_name):
     with open(file_name) as f:
          st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 
-local_css("style.css")
+local_scss("style.scss")
 
 
